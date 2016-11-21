4d6db8ef81f936bfd2dc6b7f9481a003bef00906
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-21 15:41:52 -0800

    terrible commit practices today, but it works....

--- noise.js
+++ noise.js
@@ -1,3 +1,24 @@
 $(document).ready( function() {
   // your code here
+
+  function findUrl(letter) {
+  	var foundUrl = document.getElementById(letter+"Audio").firstElementChild.src.slice(-16)
+  	return foundUrl
+  }
+
+    function makeNoise(url) {
+    	new Audio(url).play();
+  };
+
+
+  $('.note').click(function(event) {
+  	var note = $(this).html()
+  	makeNoise(findUrl(note));
+  });
+
+  $('body').keydown(function(event) {
+  	console.log(event.key);
+ 		var note = event.key.toLowerCase();
+ 		makeNoise(findUrl(note));
+  	})
 });
